3110f14b1ee0d8b21e0be83b5f8114bd0cde56b5
“jefte” <jefffraider123@gmail.com>
2025-04-16 20:16:21 -0300

    primeiros passos

---
+++ app/(project)/layout.tsx
@@ -0,0 +1,24 @@
+// import type { Metadata } from "next";
+import "./globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"]
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${poppins.className} bg-gray-50 antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
