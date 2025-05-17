f737aa875d984171457d37cad6c4017f17398f21
“jefte” <jefffraider123@gmail.com>
2025-05-17 19:51:05 -0300

    fixed bug

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,20 +1,26 @@
-import { cert, getApp, getApps, initializeApp } from "firebase-admin/app";
+import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
+// import { getStorage } from "firebase-admin/storage";
 import "server-only";
 
-const decodedKey = Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64!, "base64").toString("utf-8");
+const decodedKey = Buffer.from(
+	// biome-ignore lint/style/noNonNullAssertion: <explanation>
+	process.env.FIREBASE_PRIVATE_KEY_BASE64!,
+	"base64",
+).toString("utf-8");
 
 export const firebaseCert = cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: decodedKey,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+	privateKey: decodedKey,
 });
 
-if(!getApps().length) {
-    initializeApp({
-        credential: firebaseCert,
-    });
+if (!getApps().length) {
+	initializeApp({
+		credential: firebaseCert,
+		// storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	});
 }
 
 export const db = getFirestore();
-//export const storage = getStorage().bucket();
+// export const storage = getStorage().bucket();
